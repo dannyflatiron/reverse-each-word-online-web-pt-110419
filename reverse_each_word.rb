5eba936e093a2773fc300fe9b8b7c3f2307e8e3f
Daniel Reina <dannyflatiron@gmail.com>
2019-11-08 19:14:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ require 'pry'
 def reverse_each_word(words)
   array_words = []
   array_words = words.split(' ')
-  array_words.collect do |word|
+  array_words.map do |word|
     reversed_words = word.reverse
     binding.pry
 
