849ba6550c5699139ea1b80865b93aa848039567
Daniel Reina <dannyflatiron@gmail.com>
2019-11-08 19:12:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word(words)
   array_words = []
   array_words = words.split(' ')
   array_words.collect do |word|
-    reversed_words = word.reverse.join(' ')
+    reversed_words = word.reverse
   end
 
   # binding.pry
