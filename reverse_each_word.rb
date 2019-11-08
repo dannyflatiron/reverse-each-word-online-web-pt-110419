7a98c479b61e9ba3a426730dc0c7eccf0e706752
Daniel Reina <dannyflatiron@gmail.com>
2019-11-08 19:28:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,8 @@ require 'pry'
 def reverse_each_word(words)
   array_words = []
   array_words = words.split(' ')
-  array_words.collect do |word|
-    reversed_words = word.reverse
+  reversed_words = array_words.collect do |word|
+     word.reverse
     # binding.pry
 
   end
